6e8a45773b63ad54367b3d318098d0051ff9d447
James Tran <tmnhat2001@gmail.com>
2021-07-15 23:17:47 -0400

    Add more business search params

--- tests/param_dataclasses/business/test_search_param.py
+++ tests/param_dataclasses/business/test_search_param.py
@@ -19,10 +19,41 @@ def test_valid_location_latitude_longitude(location, latitude, longitude):
 
 
 def test_missing_location_and_latitude():
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as e:
         SearchParam(longitude=123.0)
+
+    assert (
+        str(e.value)
+        == "location must be provided. Otherwise, both latitude and longitude must be provided."
+    )
 
 
 def test_missing_location_and_longitude():
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as e:
         SearchParam(latitude=123.0)
+
+    assert (
+        str(e.value)
+        == "location must be provided. Otherwise, both latitude and longitude must be provided."
+    )
+
+
+@pytest.mark.parametrize(
+    "open_now, open_at",
+    [
+        (True, None),
+        (False, 1990),
+    ],
+)
+def test_valid_open_now_and_open_at(open_now, open_at):
+    param = SearchParam(location="Vancouver", open_now=open_now, open_at=open_at)
+
+    assert param.open_now == open_now
+    assert param.open_at == open_at
+
+
+def test_invalid_open_now_and_open_at():
+    with pytest.raises(ValueError) as e:
+        SearchParam(location="Vancouver", open_now=True, open_at=1995)
+
+    assert str(e.value) == "open_at and open_now cannot be used together."
--- yelpfusion/param_dataclasses/business/search_param.py
+++ yelpfusion/param_dataclasses/business/search_param.py
@@ -8,12 +8,27 @@ class SearchParam:
     location: Optional[str] = None
     latitude: Optional[float] = None
     longitude: Optional[float] = None
+    radius: Optional[int] = None
+    categories: Optional[str] = None
+    locale: Optional[str] = None
+    limit: Optional[int] = None
+    offset: Optional[int] = None
+    sort_by: Optional[str] = None
+    price: Optional[str] = None
+    open_now: Optional[bool] = False
+    open_at: Optional[int] = None
+    attributes: Optional[str] = None
 
     def __post_init__(self):
         self._validate_location_lat_long_presence()
+        self._validate_open_now_and_open_at()
 
     def _validate_location_lat_long_presence(self) -> None:
         if not self.location and (not self.latitude or not self.longitude):
             raise ValueError(
                 "location must be provided. Otherwise, both latitude and longitude must be provided."
             )
+
+    def _validate_open_now_and_open_at(self) -> None:
+        if self.open_now and self.open_at is not None:
+            raise ValueError("open_at and open_now cannot be used together.")
