be6a4c26b947f06c6cf85a525592149d2f73e492
James Tran <tmnhat2001@gmail.com>
2021-07-16 19:51:51 -0400

    Add wrapper for business search by phone number endpoint

---
+++ tests/endpoints/test_businesses.py
@@ -0,0 +1,17 @@
+from yelpfusion.endpoints.businesses import Businesses
+
+
+def test_business_search(requests_mock):
+    mock_api = requests_mock.get("https://api.yelp.com/v3/businesses/search", json={})
+
+    Businesses("test-key").search(location="Toronto")
+
+    assert mock_api.called is True
+
+
+def test_business_search_by_phone_number(requests_mock):
+    mock_api = requests_mock.get("https://api.yelp.com/v3/businesses/search/phone", json={})
+
+    Businesses("test-key").search_by_phone_number(phone="11234567890", locale="en_US")
+
+    assert mock_api.called is True
