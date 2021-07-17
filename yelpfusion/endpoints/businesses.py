e572bd704dce03cdec0655d9464803af957927f7
James Tran <tmnhat2001@gmail.com>
2021-07-17 00:32:14 -0400

    Create a base Endpoint class to handling request execution

--- yelpfusion/endpoints/businesses.py
+++ yelpfusion/endpoints/businesses.py
@@ -1,43 +1,25 @@
 from typing import Any, Dict, Optional
 
-import requests
-
+from yelpfusion.endpoint import Endpoint
 from yelpfusion.param_validators.business.search_param import SearchParamValidator
 
-BASE_URL = "https://api.yelp.com/v3"
 
-
-class Businesses:
-    def __init__(self, api_key: str):
-        self.api_key = api_key
-
+class Businesses(Endpoint):
     def search(self, **params: Any) -> Dict[str, Any]:
         SearchParamValidator(**params)
 
-        response = requests.get(f"{BASE_URL}/businesses/search", params=params, headers=self._request_header())
-        response.raise_for_status()
-
-        return response.json()
+        return self._get_request("/businesses/search", params)
 
     def search_by_phone_number(self, phone: str, locale: Optional[str] = None) -> Dict[str, Any]:
         params = {"phone": phone}
         if locale:
             params["locale"] = locale
 
-        response = requests.get(f"{BASE_URL}/businesses/search/phone", params=params, headers=self._request_header())
-        response.raise_for_status()
-
-        return response.json()
+        return self._get_request("/businesses/search/phone", params=params)
 
     def details(self, business_id: str, locale: Optional[str] = None) -> Dict[str, Any]:
         params = {}
         if locale:
             params["locale"] = locale
 
-        response = requests.get(f"{BASE_URL}/businesses/{business_id}", params=params, headers=self._request_header())
-        response.raise_for_status()
-
-        return response.json()
-
-    def _request_header(self) -> Dict[str, str]:
-        return {"Authorization": f"Bearer {self.api_key}"}
+        return self._get_request(f"/businesses/{business_id}", params=params)
