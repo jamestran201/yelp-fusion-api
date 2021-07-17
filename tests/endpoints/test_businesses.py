7e8f7226b4d023aebefcafc27bd87ff160d2b1ac
James Tran <tmnhat2001@gmail.com>
2021-07-17 00:35:48 -0400

    Use fixture to set up tests

--- tests/endpoints/test_businesses.py
+++ tests/endpoints/test_businesses.py
@@ -1,25 +1,32 @@
+import pytest
+
 from yelpfusion.endpoints.businesses import Businesses
 
 
-def test_business_search(requests_mock):
+@pytest.fixture
+def businesses_endpoint():
+    return Businesses("test-key")
+
+
+def test_business_search(requests_mock, businesses_endpoint):
     mock_api = requests_mock.get("https://api.yelp.com/v3/businesses/search", json={})
 
-    Businesses("test-key").search(location="Toronto")
+    businesses_endpoint.search(location="Toronto")
 
     assert mock_api.called is True
 
 
-def test_business_search_by_phone_number(requests_mock):
+def test_business_search_by_phone_number(requests_mock, businesses_endpoint):
     mock_api = requests_mock.get("https://api.yelp.com/v3/businesses/search/phone", json={})
 
-    Businesses("test-key").search_by_phone_number(phone="11234567890", locale="en_US")
+    businesses_endpoint.search_by_phone_number(phone="11234567890", locale="en_US")
 
     assert mock_api.called is True
 
 
-def test_business_details(requests_mock):
+def test_business_details(requests_mock, businesses_endpoint):
     mock_api = requests_mock.get("https://api.yelp.com/v3/businesses/fakebusiness", json={})
 
-    Businesses("test-key").details("fakebusiness", locale="en_US")
+    businesses_endpoint.details("fakebusiness", locale="en_US")
 
     assert mock_api.called is True
