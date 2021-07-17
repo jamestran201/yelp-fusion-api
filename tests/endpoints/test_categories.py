a6237a97e5affd31d2d7f7776f7bd004ba03b8c0
James Tran <tmnhat2001@gmail.com>
2021-07-17 17:37:55 -0400

    Add wrapper for categories details endpoint

--- tests/endpoints/test_categories.py
+++ tests/endpoints/test_categories.py
@@ -7,3 +7,11 @@ def test_categories_all(requests_mock):
     Categories("test-key").all(locale="fr_CA")
 
     assert mock_api.called is True
+
+
+def test_categories_details(requests_mock):
+    mock_api = requests_mock.get("https://api.yelp.com/v3/categories/ramen", json={})
+
+    Categories("test-key").details(alias="ramen", locale="fr_CA")
+
+    assert mock_api.called is True
--- yelpfusion/endpoints/categories.py
+++ yelpfusion/endpoints/categories.py
@@ -10,3 +10,10 @@ class Categories(Endpoint):
             params["locale"] = locale
 
         return self._get("/categories", params=params)
+
+    def details(self, alias: str, locale: Optional[str] = None) -> Dict[str, Any]:
+        params = {}
+        if locale:
+            params["locale"] = locale
+
+        return self._get(f"/categories/{alias}", params=params)
