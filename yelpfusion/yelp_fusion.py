18a60abb4c5edd9a4fccb1196af10acde4834160
James Tran <tmnhat2001@gmail.com>
2021-07-17 17:42:47 -0400

    Add Categories endpoints to Api class

--- yelpfusion/yelp_fusion.py
+++ yelpfusion/yelp_fusion.py
@@ -1,5 +1,6 @@
 from yelpfusion.endpoints.autocomplete import Autocomplete
 from yelpfusion.endpoints.businesses import Businesses
+from yelpfusion.endpoints.categories import Categories
 from yelpfusion.endpoints.events import Events
 from yelpfusion.endpoints.transactions import Transactions
 
@@ -8,5 +9,6 @@ class Api:
     def __init__(self, api_key: str):
         self.autocomplete = Autocomplete(api_key)
         self.businesses = Businesses(api_key)
+        self.categories = Categories(api_key)
         self.events = Events(api_key)
         self.transactions = Transactions(api_key)
