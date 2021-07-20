0a196a895962c657931309c7d6c6aacf831e9b05
James Tran <tmnhat2001@gmail.com>
2021-07-19 20:21:51 -0400

    Cache endpoints objects

--- yelpfusion/yelp_fusion.py
+++ yelpfusion/yelp_fusion.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 from yelpfusion.endpoints.autocomplete import Autocomplete
 from yelpfusion.endpoints.businesses import Businesses
 from yelpfusion.endpoints.categories import Categories
@@ -7,8 +9,29 @@ from yelpfusion.endpoints.transactions import Transactions
 
 class Api:
     def __init__(self, api_key: str):
-        self.autocomplete = Autocomplete(api_key)
-        self.businesses = Businesses(api_key)
-        self.categories = Categories(api_key)
-        self.events = Events(api_key)
-        self.transactions = Transactions(api_key)
+        self.api_key = api_key
+
+    @property  # type: ignore
+    @lru_cache(maxsize=None)
+    def autocomplete(self) -> Autocomplete:
+        return Autocomplete(self.api_key)
+
+    @property  # type: ignore
+    @lru_cache(maxsize=None)
+    def businesses(self) -> Businesses:
+        return Businesses(self.api_key)
+
+    @property  # type: ignore
+    @lru_cache(maxsize=None)
+    def categories(self) -> Categories:
+        return Categories(self.api_key)
+
+    @property  # type: ignore
+    @lru_cache(maxsize=None)
+    def events(self) -> Events:
+        return Events(self.api_key)
+
+    @property  # type: ignore
+    @lru_cache(maxsize=None)
+    def transactions(self) -> Transactions:
+        return Transactions(self.api_key)
